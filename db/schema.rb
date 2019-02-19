81e75b86e9dfa23e8844685706ec22ad7c200fcf
yoshi <yoshi@jp>
2019-02-19 19:15:53 +0000

    db re_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190217111021) do
+ActiveRecord::Schema.define(version: 20190219190305) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.integer  "user_id"
-    t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
+    t.integer  "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
