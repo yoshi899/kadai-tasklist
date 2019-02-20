bf6301d14b973dfb680581381c44bbd4d94fbd37
yoshi <yoshi@jp>
2019-02-20 15:35:56 +0000

    review2-2_code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+    # review2-2の重複ユーザの排除を追加
     validates :name, presence: true, length: { maximum: 50 }
+    validates :name, uniqueness:true
     has_secure_password
     
     has_many :tasks
