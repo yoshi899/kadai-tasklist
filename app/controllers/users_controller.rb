31707e0d3d3c320f8047b8be82d37c8ddc6b2bfc
yoshi <yoshi@jp>
2019-02-20 13:26:17 +0000

    review2-1_code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :require_user_logged_in, except: [:new]
+  # before_action :require_user_logged_in, only: [:index, :show, :new, :create]
 
   def new
     @user = User.new
